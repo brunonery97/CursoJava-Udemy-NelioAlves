8c564c74110ee4b92334c7baaa1cb32bbbebabd1
brunonery97 <bruninnery97@gmail.com>
2025-11-25 10:14:14 -0300

    Subindo a sessao14 ExpressoesLambda

---
+++ sessao14-ProgramacaoFuncionaleExpressoesLambda/ExperienciaComComparator/src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import entities.Product;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("Tv", 900.0));
+        list.add(new Product("Notebook", 1200.0));
+        list.add(new Product("Tablet", 450.0));
+
+        //expressão LAMBDA do Comparator
+        list.sort((p1,p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+
+        for (Product p : list){
+            System.out.println(p);
+        }
+    }
+}
